23c4e4aefbc8b1999ea137a15a380b1f5340df35
Abramova Nina <abramova.nina.g@gmail.com>
2023-09-02 13:21:05 +0300

    ci: add cors

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,30 @@ const mongoose = require('mongoose');
 const NotFoundError = require('./errors/not-found-err');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 const app = express();
 app.use(requestLogger);
+app.use((req, res, next) => {
+  const { method } = req;
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+  }
+
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  return next();
+});
+
 const port = 3000;
 
 const { login, createUser } = require('./controllers/user');
